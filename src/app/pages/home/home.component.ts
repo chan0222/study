c41652d18feb934803a8f0999dafe7936e6c6d67
곽찬영 <gksmftor8745@naver.com>
2024-04-02 14:18:23 +0900

    input 없이 기본 기능- 추가 삭제

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+
+type Todo = {
+  id: number;
+  title: string;
+  done: boolean;
+};
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  inputValue: string = '';
+  Todo: [] = [];
+  todos: Todo[] = [
+    {
+      id: 1,
+      title: ' 밥먹기',
+      done: false,
+    },
+    {
+      id: 2,
+      title: '눕기',
+      done: false,
+    },
+  ];
+
+  addtodo() {
+    if (!this.inputValue) {
+      alert('입력하신후 눌러주세요!');
+    }
+    const lastItem = this.todos[this.todos.length - 1].id;
+
+    const newTodo = {
+      id: lastItem + 1,
+      title: this.inputValue,
+      done: false,
+    };
+    this.todos.push(newTodo);
+  }
+
+  remove(todos: Todo) {
+    this.todos.filter((item) => item.id !== todos.id);
+  }
+}
